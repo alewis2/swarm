1d9cfc7bb0835a8453dd3c0e16f9720c60f9f2f3
Aaron Lewis <alewis2@umbc.edu>
2018-05-23 19:09:31 -0400

    Created menus and learned ui

--- Swarm/Assets/Scripts/Boss/ShootBullet.cs
+++ Swarm/Assets/Scripts/Boss/ShootBullet.cs
@@ -37,7 +37,7 @@ public class ShootBullet : MonoBehaviour
         //stop shooting if boss is "dead"
         if (gameObject.transform.position.y > 0) {
 
-            //reset and add new ghost
+            //reset counters on restart
             if (Input.GetKeyDown(KeyCode.R)) {
                 aoeCooldown = 0;
                 burstDelay = burstReset;
@@ -51,24 +51,20 @@ public class ShootBullet : MonoBehaviour
             if (burstDelay <= 1) {
                 if (delay <= 0) {
                     //The Bullet instantiation happens here.
-                    GameObject Temporary_Bullet_Handler;
+                    GameObject bullet;
 
-                    Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
-
-                    //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
-                    //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
-
-                    Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+                    bullet = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+                    bullet.transform.Rotate(Vector3.left * 90);
 
                     //Retrieve the Rigidbody component from the instantiated Bullet and control it.
                     Rigidbody Temporary_RigidBody;
-                    Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+                    Temporary_RigidBody = bullet.GetComponent<Rigidbody>();
 
                     //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
                     Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
 
                     //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
-                    Destroy(Temporary_Bullet_Handler, 10.0f);
+                    Destroy(bullet, 10.0f);
                     delay = resetDelay;
                 }
             }
@@ -95,18 +91,18 @@ public class ShootBullet : MonoBehaviour
 
     void aoeBurst(int offsetAngle) {
 
-        GameObject Temporary_Bullet_Handler;
+        GameObject bullet;
 
-        Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+        bullet = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
 
-        Temporary_Bullet_Handler.transform.Rotate(new Vector3(-90,0,0 + offsetAngle));
+        bullet.transform.Rotate(new Vector3(-90,0,0 + offsetAngle));
 
         Rigidbody Temporary_RigidBody;
-        Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+        Temporary_RigidBody = bullet.GetComponent<Rigidbody>();
 
-        Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force/2);
+        Temporary_RigidBody.AddForce(bullet.transform.rotation * bullet.transform.forward * Bullet_Forward_Force/2);
 
-        Destroy(Temporary_Bullet_Handler, 2.0f);
+        Destroy(bullet, 5.0f);
     }
 
 
---
+++ Swarm/Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void GotoMainMenu() {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void GotoTutorial() {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void GotoCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void GotoLevel1() {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void GotoLevel2() {
+        SceneManager.LoadScene("Level 2");
+    }
+
+	/*
+    public void GotoCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+    public void GotoCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+    public void GotoCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+	*/
+}
